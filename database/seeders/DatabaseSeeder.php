2a4c535ba6781ea4e310ed78b0ba5fd04124209a
LaupWing <loc_strict@hotmail.com>
2023-06-24 17:43:24 +0200

    udapte

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,16 @@ class DatabaseSeeder extends Seeder
    {
       \App\Models\User::factory(100)->create();
       \App\Models\Genre::factory(10)->create();
-      \App\Models\Follower::factory(200)->create();
+
+      $totalFollowers = 500;
+      for($i =0; $i < $totalFollowers; $i++){
+         $follower = \App\Models\Follower::factory()->make();
+         
+         while(\App\Models\Follower::where("user_id", $follower->user_id)->where("genre_id", $follower->genre_id)->exists()){
+            $follower = \App\Models\Follower::factory()->make();
+         }
+         $follower->save();
+      }
 
       // \App\Models\User::factory()->create([
       //     'name' => 'Test User',
