b430538abd3f71c0525f3fe4891478e9196383d5
LaupWing <loc_strict@hotmail.com>
2023-06-24 17:12:58 +0200

    update

---
+++ database/factories/FollowerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Genre;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Follower>
+ */
+class FollowerFactory extends Factory
+{
+   /**
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
+   public function definition(): array
+   {
+      $userIds = User::pluck('id')->toArray();
+      $genreIds = Genre::pluck('id')->toArray();
+
+      return [
+         "user_id" => $this->faker->unique()->randomElement($userIds),
+         "genre_id" => $this->faker->unique()->randomElement($genreIds),
+      ];
+   }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
    {
       \App\Models\User::factory(10)->create();
       \App\Models\Genre::factory(10)->create();
+      \App\Models\Follower::factory(50)->create();
 
       // \App\Models\User::factory()->create([
       //     'name' => 'Test User',
