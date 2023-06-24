b52a56bb3ad9cb9221b1779f3feab53ccd5c4bdb
LaupWing <loc_strict@hotmail.com>
2023-06-24 16:41:49 +0200

    follower

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Follower extends Model
 {
    use HasFactory;
+
+   protected $fillable = [
+      "user_id",
+      "genre_id"
+   ];
+
+   public function user()
+   {
+      return $this->belongsTo(User::class);
+   }
+
+   public function genre()
+   {
+      return $this->belongsTo(Genre::class);
+   }
 }
