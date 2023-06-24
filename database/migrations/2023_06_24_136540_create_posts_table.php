137807fb6ec6a98ce91d5b7374773113fe5828d6
LaupWing <loc_strict@hotmail.com>
2023-06-24 16:19:43 +0200

    udpate

--- database/migrations/2023_06_24_136540_create_posts_table.php
+++ database/migrations/2023_06_24_136540_create_posts_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
       Schema::create("posts", function (Blueprint $table) {
          $table->id();
          $table->timestamps();
+         $table->foreignIdFor(
+            \App\Models\Genre::class,
+            "genre_id"
+         )->constrained("genres")->onDelete("cascade");
+         $table->foreignIdFor(
+            \App\Models\User::class,
+            "user_id"
+         )->constrained("users")->onDelete("cascade");
       });
    }
 
