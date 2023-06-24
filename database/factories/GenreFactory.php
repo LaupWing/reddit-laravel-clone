3c93b02cf058570fb044b5cc564228e2b0aeeafa
LaupWing <loc_strict@hotmail.com>
2023-06-24 17:04:48 +0200

    udpate

---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Genre>
+ */
+class GenreFactory extends Factory
+{
+   /**
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
+   public function definition(): array
+   {
+      return [
+         "title" => fake()->unique()->name(),
+         "description" => fake()->text(),
+         "user_id" => function() {
+            return User::inRandomOrder()->first()->id;
+         }
+      ];
+   }
+}
