9c4ccd218db704c3efaea6feb8094c620519736a
LaupWing <loc_strict@hotmail.com>
2023-06-24 16:10:35 +0200

    update

--- database/migrations/2023_06_24_140739_create_followers_table.php
+++ database/migrations/2023_06_24_140739_create_followers_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
       Schema::create("followers", function (Blueprint $table) {
          $table->id();
          $table->timestamps();
+         $table->foreignIdFor(
+            \App\Models\User::class,
+            "user_id"
+         )->constrained("users")->onDelete("cascade");
       });
    }
 
