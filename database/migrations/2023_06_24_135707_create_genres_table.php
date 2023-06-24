5232e34beb147e2f09bd3b2be0649356acff5c0f
LaupWing <loc_strict@hotmail.com>
2023-06-24 16:32:41 +0200

    udpate

--- database/migrations/2023_06_24_135707_create_genres_table.php
+++ database/migrations/2023_06_24_135707_create_genres_table.php
@@ -6,30 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create("genres", function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string("name")->unique();
-            $table->string("description");
-            $table->string("profile_picture")->nullable();
-            $table->string("thumbnail")->nullable();
-            $table->foreignIdFor(
-               \App\Models\User::class,
-               "user_id"
-            )->constrained("users")->onDelete("cascade");
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("genres", function (Blueprint $table) {
+         $table->id();
+         $table->timestamps();
+         $table->string("name")->unique();
+         $table->string("description");
+         $table->string("profile_picture")->nullable();
+         $table->string("thumbnail")->nullable();
+         $table->foreignIdFor(
+            \App\Models\User::class,
+            "user_id"
+         )->constrained("users")->onDelete("cascade");
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists("genres");
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("genres");
+   }
 };
