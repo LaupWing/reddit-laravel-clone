a0ca65b5d682d3d6a5417ef46587bf63c80476bb
LaupWing <loc_strict@hotmail.com>
2023-06-24 15:57:12 +0200

    added genre

--- database/migrations/2023_06_24_135537_create_posts_table.php
+++ database/migrations/2023_06_24_135537_create_posts_table.php
@@ -6,22 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('posts', function (Blueprint $table) {
+         $table->id();
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('posts');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('posts');
+   }
 };
