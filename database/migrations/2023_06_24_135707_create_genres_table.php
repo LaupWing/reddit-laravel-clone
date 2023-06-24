528398ccc7ff523323b4fae90ba0627f733d085b
LaupWing <loc_strict@hotmail.com>
2023-06-24 15:59:56 +0200

    udpate

--- database/migrations/2023_06_24_135707_create_genres_table.php
+++ database/migrations/2023_06_24_135707_create_genres_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('genres', function (Blueprint $table) {
+        Schema::create("genres", function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("name")->unique();
+            $table->string("description");
+            // $table-
         });
     }
 
