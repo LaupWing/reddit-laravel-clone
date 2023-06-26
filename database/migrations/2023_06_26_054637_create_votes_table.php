8253ead0b9a5a742b7389186243c668b701011ad
LaupWing <loc_strict@hotmail.com>
2023-06-26 07:56:21 +0200

    Votes

--- database/migrations/2023_06_26_054637_create_votes_table.php
+++ database/migrations/2023_06_26_054637_create_votes_table.php
@@ -6,22 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('votes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("votes", function (Blueprint $table) {
+         $table->primary(["topic_id", "user_id"]);
+         $table->timestamps();
+         $table->foreignIdFor(
+            \App\Models\User::class,
+            "user_id"
+         )->constrained("users")->onDelete("cascade");
+         $table->foreignIdFor(
+            \App\Models\Topic::class,
+            "topic_id"
+         )->constrained("topics")->onDelete("cascade");
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('votes');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("votes");
+   }
 };
