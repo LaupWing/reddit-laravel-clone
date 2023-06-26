b0314627a9b76bc38f1cfdeb7cf5909281d544b1
LaupWing <loc_strict@hotmail.com>
2023-06-26 08:52:47 +0200

    postfavorites

--- database/migrations/2023_06_26_055102_create_post_favorites_table.php
+++ database/migrations/2023_06_26_055102_create_post_favorites_table.php
@@ -6,22 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('post_favorites', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("post_favorites", function (Blueprint $table) {
+         $table->id();
+         $table->foreignIdFor(
+            \App\Models\User::class,
+            "user_id"
+         )->constrained("users")->onDelete("cascade");
+         $table->foreignIdFor(
+            \App\Models\Topic::class,
+            "topic_id"
+         )->constrained("topics")->onDelete("cascade");
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('post_favorites');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("post_favorites");
+   }
 };
