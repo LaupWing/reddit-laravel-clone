422a10d91c93e45a10c767ba12d263585d028ce0
LaupWing <loc_strict@hotmail.com>
2023-06-26 07:32:48 +0200

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,7 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <title>Laravel</title>
-
-   <!-- Fonts -->
+   <title>Reddit Clone Laravel</title>
    <link rel="preconnect" href="https://fonts.bunny.net">
    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
