f61b68734d2c93b7c467b2360fa2e7a8b6a5e654
LaupWing <loc_strict@hotmail.com>
2023-06-26 07:43:55 +0200

    media

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
    use HasFactory;
 
-   protected $fillable = ["title", "content"];
+   protected $fillable = ["title", "content", "media"];
 
    public function topic()
    {
--- database/migrations/2023_06_24_136540_create_posts_table.php
+++ database/migrations/2023_06_24_136540_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
       Schema::create("posts", function (Blueprint $table) {
          $table->id();
          $table->timestamps();
+         $table->string("media")->nullable();
          $table->foreignIdFor(
             \App\Models\Topic::class,
             "topic_id"
