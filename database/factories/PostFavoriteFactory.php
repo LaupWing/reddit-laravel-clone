3086d617d988c153d4df0730fe4f62a4e79f2055
LaupWing <loc_strict@hotmail.com>
2023-06-27 06:56:01 +0200

    favorite

---
+++ database/factories/PostFavoriteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PostFavorite>
+ */
+class PostFavoriteFactory extends Factory
+{
+   /**
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
+   public function definition(): array
+   {
+      $user = \App\Models\User::inRandomOrder()->first();
+      $post = \App\Models\Post::inRandomOrder()->first();
+
+      return [
+         "user_id" => $user->id,
+         "post_id" => $post->id
+      ];
+   }
+}
