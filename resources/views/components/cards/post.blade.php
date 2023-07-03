07b3dfe01659a943588bd2b97ebb243e8006d1ed
LaupWing <loc_strict@hotmail.com>
2023-07-03 11:41:07 +0200

    udpate

--- resources/views/components/cards/post.blade.php
+++ resources/views/components/cards/post.blade.php
@@ -1,10 +1,12 @@
 <div 
-   {{$attributes->merge(["class"=> "bg-secondary flex items-center border border-line rounded"])}}
+   {{$attributes->merge(["class"=> "bg-secondary flex border border-line rounded"])}}
 >
    <div class="flex flex-col bg-gray-100 text-gray-400 p-2 gap-1 items-center">
       <x-icons.arrow-up-circle class="w-6 h-6" />
       100
       <x-icons.arrow-down-circle class="w-6 h-6" />
    </div>
-   
+   <div>
+      <h2>{{$post->name}}</h2>
+   </div>
 </div>
