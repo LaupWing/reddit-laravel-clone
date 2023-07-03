2a1d7ccc693e88ce65160d242636ce796a41b7b4
LaupWing <loc_strict@hotmail.com>
2023-07-03 10:44:22 +0200

    udpate

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
    public function index()
    {
-      return view("index");
+      return view("index", [
+         "posts" => Post::latest()->get()
+      ]);
    }
 }
