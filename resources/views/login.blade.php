995bc2848c8d00412bc0ff7869a9d195a44089ed
LaupWing <loc_strict@hotmail.com>
2023-07-04 18:18:49 +0200

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,23 @@
 <x-layout>
-   <div class="mx-auto mt-6 bg-white rounded shadow p-6 w-full max-w-md">
-
+   <div class="mx-auto mt-6 gap-6 bg-white flex flex-col rounded shadow px-6 py-8 w-full max-w-sm">
+      <div class="flex flex-col">
+         <h2 class="ml-1 font-bold text-gray-300 uppercase tracking-wide">Email</h2>
+         <input 
+            class="border h-10 flex-1 bg-gray-100 border-gray-200 focus:outline-none rounded px-2"
+            placeholder="Create post"
+            type="text"
+         >
+      </div>
+      <div class="flex flex-col">
+         <h2 class="ml-1 font-bold text-gray-300 uppercase tracking-wide">Password</h2>
+         <input 
+            class="border h-10 flex-1 bg-gray-100 border-gray-200 focus:outline-none rounded px-2"
+            placeholder="Password"
+            type="password"
+         >
+      </div>
+      <button class="bg-accent w-32 my-1 mx-auto text-white rounded-full py-1 font-bold">
+         Log In
+      </button>
    </div>
 </x-layout>
