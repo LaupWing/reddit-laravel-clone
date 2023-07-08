90ef4bd5529fb92e6e68b25b6f3449eaf06becd2
LaupWing <loc_strict@hotmail.com>
2023-07-08 16:47:49 +0200

    added header

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -6,13 +6,16 @@
             src="{{$topic->thumbnail ? $topic->thumbnail : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}}" 
             alt="Profile"
          >
-         <div class="absolute left-10 bottom-0">
+      </div>
+      <div class="bg-white p-4 relative">
+         <div class="absolute left-10 top-0">
             <img 
-               class="w-24 h-24 rounded-full border-gray-300 border-[5px] transform translate-y-1/2 object-cover"
+               class="w-24 h-24 rounded-full border-gray-300 border-[5px] transform -translate-y-1/2 object-cover"
                src="{{$topic->profile_picture ? $topic->profile_picture : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}}" 
                alt="Profile"
             >
          </div>
+         <h2 class="ml-32 text-3xl font-bold">{{$topic->name}} <button>Join</button></h2>
       </div>
    </div>
 </x-layout>
