cbb9aae3eaec58a85519bd889563371bebbb0e3d
LaupWing <loc_strict@hotmail.com>
2023-07-08 16:45:25 +0200

    added thumnila

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -1,11 +1,18 @@
 <x-layout>
    <div class="w-full flex flex-col">
-      <div class="max-h-60 h-[30vh]">
+      <div class="max-h-60 h-[30vh] relative">
          <img 
             class="h-full w-full object-cover"
-            src="{{$topic->profile_picture ? $topic->profile_picture : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}}" 
+            src="{{$topic->thumbnail ? $topic->thumbnail : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}}" 
             alt="Profile"
          >
+         <div class="absolute left-10 bottom-0">
+            <img 
+               class="w-24 h-24 rounded-full border-gray-300 border-[5px] transform translate-y-1/2 object-cover"
+               src="{{$topic->profile_picture ? $topic->profile_picture : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'}}" 
+               alt="Profile"
+            >
+         </div>
       </div>
    </div>
 </x-layout>
