42f8f073e525d06517861c91bc3302915847fe27
LaupWing <loc_strict@hotmail.com>
2023-07-08 17:51:33 +0200

    have split up the pollicy into the cards

---
+++ resources/views/components/cards/policy.blade.php
@@ -0,0 +1,29 @@
+<x-cards.base>
+   <div class="flex flex-col text-sm">
+      <div class="grid grid-cols-2 flex-1">
+         <ul class="flex flex-col gap-2">
+            <li class="leading-5">User Agreement</li>
+            <li class="leading-5">Privacy Policy</li>
+         </ul>
+         <ul class="flex flex-col gap-2">
+            <li class="leading-5">Content Policy</li>
+            <li class="leading-5">Moderator Code of Conduct</li>
+         </ul>
+      </div>
+      <hr class="my-4">
+      <div class="grid grid-cols-2 flex-1">
+         <ul class="flex flex-col gap-2">
+            <li class="leading-5">English</li>
+            <li class="leading-5">Français</li>
+            <li class="leading-5">Italiano</li>
+         </ul>
+         <ul class="flex flex-col gap-2">
+            <li class="leading-5">Deutsch</li>
+            <li class="leading-5">Español</li>
+            <li class="leading-5">Português</li>
+         </ul>
+      </div>
+      <hr class="my-4">
+      <span>Reddit, Inc. © 2023. All rights reserved.</span>
+   </div>
+</x-cards.base>
