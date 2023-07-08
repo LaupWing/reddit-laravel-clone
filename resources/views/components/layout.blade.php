e6771cda048e0f36ca4e25c6d031832e7cd867dc
LaupWing <loc_strict@hotmail.com>
2023-07-08 19:03:09 +0200

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <title>Reddit Clone Laravel</title>
+   <title>@yield("title", "Reddit Clone Laravel")</title>
    <link rel="preconnect" href="https://fonts.bunny.net">
    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
    @vite("resources/css/app.css")
